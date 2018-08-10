0ae3e783c6264d0ba8824141aca143667134de5a
malik nadal <mnadal@cisphl.org>
2018-08-10 14:12:25 +0000

    e

---
+++ css/product.js
@@ -0,0 +1,29 @@
+function myfunction() {
+  
+    var message = document.getElementById("message").value;
+    var email = document.getElementById("email").value;
+    var name = document.getElementById("name").value;
+    var check1 = document.getElementById("check1").checked;
+   
+    
+ 
+
+}
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if(form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
